a42a72aa63c6b2a875c5d67f5fb2c987be644712
Andi-A <Andi-A>
2023-03-22 00:50:06 +0100

    CRUD

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -41,7 +41,7 @@ exports.getTour = async (req, res) => {
 
 exports.createTour = async (req, res) => {
   try {
-    const newTour = await Tour.create(req.body); // This mehtod returns a promise
+    const newTour = await Tour.create(req.body); // This method returns a promise
 
     res.status(201).json({
       status: 'success',
@@ -57,20 +57,40 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: {
-      tour: 'Updated tour',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
+exports.deleteTour = async (req, res) => {
   // const id = req.params.id * 1;
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
 
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
