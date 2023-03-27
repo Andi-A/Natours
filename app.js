5be319e4bade5bbd59aa18575a18f6e7a80cdfee
Andi-A <Andi-A>
2023-03-27 17:04:11 +0200

    Create Review models

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+// eslint-disable-next-line import/no-unresolved
+const reviewRouter = require('./routes/reviewRoutes');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
@@ -66,6 +68,7 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404)); // This jumps to the error middleware down below
